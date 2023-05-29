2c21c0042dc0af2df46198fb3b36b85590fe34e7
Celcius <itscelciusgaming@gmail.com>
2023-05-29 10:51:10 +0800

    may 2023 rates & gen charge update

--- src/components/GenRates.jsx
+++ src/components/GenRates.jsx
@@ -10,19 +10,19 @@ const Rates = () => {
   return (
     <section>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of April 2023
+        Breakdown Of Generation Charge For The Month Of May 2023
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[0]} alt="rate" />
       </div>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of March 2023
+        Breakdown Of Generation Charge For The Month Of April 2023
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[1]} alt="rate" />
       </div>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of February 2023
+        Breakdown Of Generation Charge For The Month Of March 2023
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[2]} alt="rate" />
