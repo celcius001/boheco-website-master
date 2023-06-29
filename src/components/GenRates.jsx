15eb5f962af5e184e18d7005da34196aa0fec5a4
Celcius <itscelciusgaming@gmail.com>
2023-06-29 08:24:33 +0800

    june 2023 rates & gen charge update

--- src/components/GenRates.jsx
+++ src/components/GenRates.jsx
@@ -6,23 +6,25 @@ const img = [
   "assets/rates/gr2.png",
 ];
 
+const months = ["June", "May", "April"];
+
 const Rates = () => {
   return (
     <section>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of May 2023
+        Breakdown Of Generation Charge For The Month Of {months[0]} 2023
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[0]} alt="rate" />
       </div>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of April 2023
+        Breakdown Of Generation Charge For The Month Of {months[1]} 2023
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[1]} alt="rate" />
       </div>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of March 2023
+        Breakdown Of Generation Charge For The Month Of {months[2]} 2023
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[2]} alt="rate" />
