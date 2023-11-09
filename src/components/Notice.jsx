9ecce0b34d6c98cbee900b017c4c84b0625c1fed
Celcius <itscelciusgaming@gmail.com>
2023-11-09 08:25:46 +0800

    agmm notice added

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -46,6 +46,8 @@ const noticeTwo = [
   "assets/noticetwo/17.jpg",
 ];
 
+const agmm = ["assets/agmm/01.jpg", "assets/agmm/02.jpg"];
+
 const noticeOneArray = [
   noticeOne[0],
   noticeOne[1],
@@ -91,12 +93,16 @@ const noticeTwoArray = [
   noticeTwo[16],
 ];
 
+const agmmArray = [agmm[0], agmm[1]];
+
 let noticeCountOne = 0;
 let noticeCountTwo = 0;
+let agmmCount = 0;
 
 const Notice = () => {
   const [noticeOneIndex, setNoticeOneIndex] = useState(0);
   const [noticeTwoIndex, setNoticeTwoIndex] = useState(0);
+  const [agmmIndex, setAgmmIndex] = useState(0);
 
   const slideRef = useRef();
 
@@ -108,32 +114,86 @@ const Notice = () => {
     slideRef.current.addEventListener("animationend", removeAnimation);
   });
 
-  const handleOnNextCLickOne = () => {
-    noticeCountOne = (noticeCountOne + 1) % noticeOne.length;
-    setNoticeOneIndex(noticeCountOne);
-    slideRef.current.classList.add("fade-anim");
-  };
-  const handleOnPreviousCLickOne = () => {
-    const rateLength = noticeOne.length;
-    noticeCountOne = (noticeOneIndex + rateLength - 1) % rateLength;
-    setNoticeOneIndex(noticeCountOne);
-    slideRef.current.classList.add("fade-anim");
+  // * Function called when the button is clicked on next
+  const handleNext = (e) => {
+    const id = e.currentTarget.id;
+    if (id === "agmm-next") {
+      agmmCount = (agmmCount + 1) % agmm.length;
+      setAgmmIndex(agmmCount);
+      slideRef.current.classList.add("fade-anim");
+    }
+    if (id === "notice-one-next") {
+      noticeCountOne = (noticeCountOne + 1) % noticeOne.length;
+      setNoticeOneIndex(noticeCountOne);
+      slideRef.current.classList.add("fade-anim");
+    }
+    if (id === "notice-two-next") {
+      noticeCountTwo = (noticeCountTwo + 1) % noticeTwo.length;
+      setNoticeTwoIndex(noticeCountTwo);
+      slideRef.current.classList.add("fade-anim");
+    }
   };
 
-  const handleOnNextCLickTwo = () => {
-    noticeCountTwo = (noticeCountTwo + 1) % noticeTwo.length;
-    setNoticeTwoIndex(noticeCountTwo);
-    slideRef.current.classList.add("fade-anim");
-  };
-  const handleOnPreviousCLickTwo = () => {
-    const rateLength = noticeTwo.length;
-    noticeCountTwo = (noticeTwoIndex + rateLength - 1) % rateLength;
-    setNoticeTwoIndex(noticeCountTwo);
-    slideRef.current.classList.add("fade-anim");
+  // * Function called when the button is clicked on previous
+  const handlePrev = (e) => {
+    const id = e.currentTarget.id;
+    if (id === "agmm-prev") {
+      const rateLength = agmm.length;
+      agmmCount = (agmmIndex + rateLength - 1) % rateLength;
+      setAgmmIndex(agmmCount);
+      slideRef.current.classList.add("fade-anim");
+    }
+    if (id === "notice-one-prev") {
+      const rateLength = noticeOne.length;
+      noticeCountOne = (noticeOneIndex + rateLength - 1) % rateLength;
+      setNoticeOneIndex(noticeCountOne);
+      slideRef.current.classList.add("fade-anim");
+    }
+    if (id === "notice-two-prev") {
+      const rateLength = noticeTwo.length;
+      noticeCountTwo = (noticeTwoIndex + rateLength - 1) % rateLength;
+      setNoticeTwoIndex(noticeCountTwo);
+      slideRef.current.classList.add("fade-anim");
+    }
   };
 
   return (
     <div>
+      <section>
+        <div className="flex items-center justify-center text-xl py-2 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-2 uppercase">
+          41<sup className="font-light">st</sup> Annual General Membership
+          Metting (AGMM)
+        </div>
+        <div
+          ref={slideRef}
+          className="w-full select-one py-2 flex justify-center items-center relative"
+        >
+          <div>
+            <img
+              className="md:h-[900px] h-[720]"
+              draggable={false}
+              src={agmmArray[agmmIndex]}
+              alt="rate"
+            />
+          </div>
+          <div className="absolute w-full top-1/2 trasform translate-y-1/2 py-2 flex justify-between items-center">
+            <button
+              id="agmm-prev"
+              className="bg-black text-white p-1 px-4 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition-all duration-300"
+              onClick={handlePrev}
+            >
+              <AiOutlineVerticalRight size={30} />
+            </button>
+            <button
+              id="agmm-next"
+              className="bg-black text-white p-1 px-4 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition-all duration-300"
+              onClick={handleNext}
+            >
+              <AiOutlineVerticalLeft size={30} />
+            </button>
+          </div>
+        </div>
+      </section>
       <section>
         <div className="flex items-center justify-center text-xl py-2 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-2 uppercase">
           ERC CASE NO. 2023-113 RC ORDER/ 10 OCTOBER 2023
@@ -152,14 +212,16 @@ const Notice = () => {
           </div>
           <div className="absolute w-full top-1/2 trasform translate-y-1/2 py-2 flex justify-between items-center">
             <button
+              id="notice-one-prev"
               className="bg-black text-white p-1 px-4 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition-all duration-300"
-              onClick={handleOnPreviousCLickOne}
+              onClick={handlePrev}
             >
               <AiOutlineVerticalRight size={30} />
             </button>
             <button
+              id="notice-one-next"
               className="bg-black text-white p-1 px-4 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition-all duration-300"
-              onClick={handleOnNextCLickOne}
+              onClick={handleNext}
             >
               <AiOutlineVerticalLeft size={30} />
             </button>
@@ -184,14 +246,16 @@ const Notice = () => {
           </div>
           <div className="absolute w-full top-1/2 trasform translate-y-1/2 py-2 flex justify-between items-center">
             <button
+              id="notice-two-prev"
               className="bg-black text-white p-1 px-4 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition-all duration-300"
-              onClick={handleOnPreviousCLickTwo}
+              onClick={handlePrev}
             >
               <AiOutlineVerticalRight size={30} />
             </button>
             <button
+              id="notice-two-next"
               className="bg-black text-white p-1 px-4 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition-all duration-300"
-              onClick={handleOnNextCLickTwo}
+              onClick={handleNext}
             >
               <AiOutlineVerticalLeft size={30} />
             </button>
