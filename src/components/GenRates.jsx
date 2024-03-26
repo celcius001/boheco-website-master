56fe076af3ff038130829e26561507bac84099bc
celcius001 <itscelciusgaming@gmail.com>
2024-03-26 09:25:22 +0800

    march 2024 rates update

--- src/components/GenRates.jsx
+++ src/components/GenRates.jsx
@@ -6,7 +6,7 @@ const img = [
   "assets/rates/gr2.png",
 ];
 
-const months = ["February", "January", "December"];
+const months = ["March", "February", "January"];
 
 const Rates = () => {
   return (
