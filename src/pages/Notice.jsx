ac55b5cd6a7b9f42ec06ac6b95e2f261eace7359
celcius001 <itscelciusgaming@gmail.com>
2024-06-24 10:53:21 +0800

    add NoticeBid2

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Notice from "../components/Notice";
 import NoticeBid from "../components/NoticeBid";
+import NoticeBid2 from "../components/NoticeBid2";
 
 function Advisory() {
   return (
@@ -10,6 +11,13 @@ function Advisory() {
           <div className="text-center mb-12">
             <div className="text-4xl font-extrabold py-2 uppercase">
               <NoticeBid />
+            </div>
+          </div>
+        </section>
+        <section className="container px-5 py-24 mx-auto">
+          <div className="text-center mb-12">
+            <div className="text-4xl font-extrabold py-2 uppercase">
+              <NoticeBid2 />
             </div>
           </div>
         </section>
