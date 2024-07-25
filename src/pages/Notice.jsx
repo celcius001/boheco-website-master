986476088cedf527e2a15e09932d65c664defa17
celcius001 <itscelciusgaming@gmail.com>
2024-07-25 18:41:31 +0800

    added rph6

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -5,11 +5,19 @@ import NoticeBid2 from "../components/NoticeBid2";
 import NoticeBid3 from "../components/NoticeBid3";
 import NoticeBid4 from "../components/NoticeBid4";
 import NoticeBid5 from "../components/NoticeBid5";
+import NoticeBid6 from "../components/NoticeBid6";
 
 function Advisory() {
   return (
     <>
       <div className="bg-image2 flex flex-col items-center justify-center">
+        <section className="container px-5 py-24 mx-auto">
+          <div className="text-center mb-12">
+            <div className="text-4xl font-extrabold py-2 uppercase">
+              <NoticeBid6 />
+            </div>
+          </div>
+        </section>
         <section className="container px-5 py-24 mx-auto">
           <div className="text-center mb-12">
             <div className="text-4xl font-extrabold py-2 uppercase">
