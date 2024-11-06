9edf66e6742c9a4d3005bcef8df49d79162867dd
celcius001 <itscelciusgaming@gmail.com>
2024-11-06 14:41:07 +0800

    added CORSeal

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const privacyNotice = "assets/privacy/01.png";
+const corseal = "assets/privacy/CORSeal_BOHECOII.png";
 
 const Privacy = () => {
   return (
@@ -10,12 +11,22 @@ const Privacy = () => {
           <div className="text-center mb-12">
             <div className="text-4xl font-extrabold py-2 uppercase">
               <div className="flex flex-col items-center justify-center text-xl py-2 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-2 uppercase">
-                Privacy Notice
+                <img
+                  className="my-4"
+                  draggable={false}
+                  src={corseal}
+                  alt="corseal"
+                  width={250}
+                  height={250}
+                />
+                <div>Privacy Notice</div>
                 <div className="bg-white p-8 rounded-lg shadow-lg mt-4">
                   <img
                     draggable={false}
                     src={privacyNotice}
                     alt="privacyNotice"
+                    height={1020}
+                    width={720}
                   />
                 </div>
               </div>
