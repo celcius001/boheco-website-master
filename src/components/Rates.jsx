666ad913b32030adc4ca1cff3ceb7c8e3ad658ce
celcius001 <itscelciusgaming@gmail.com>
2025-01-22 08:12:21 +0800

    update power rates jan 2025

--- src/components/Rates.jsx
+++ src/components/Rates.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const img = [
-  "assets/rates/dec2024/slide1.jpg",
-  "assets/rates/dec2024/slide2.jpg",
-  "assets/rates/dec2024/slide3.jpg",
-  "assets/rates/dec2024/slide4.jpg",
-  "assets/rates/dec2024/slide5.jpg",
+  "assets/rates/jan2025/slide1.jpg",
+  "assets/rates/jan2025/slide2.jpg",
+  "assets/rates/jan2025/slide3.jpg",
+  "assets/rates/jan2025/slide4.jpg",
 ];
 
 const Rates = () => {
@@ -15,36 +14,15 @@ const Rates = () => {
         Power Rate Advisory
       </div>
       <div className="flex gap-4 flex-col items-center justify-center">
-        <img
-          className="bg-white p-2 rounded-md shadow-lg md:w-3/5"
-          draggable={false}
-          src={img[0]}
-          alt="rate"
-        />
-        <img
-          className="bg-white p-2 rounded-md shadow-lg md:w-3/5"
-          draggable={false}
-          src={img[1]}
-          alt="rate"
-        />
-        <img
-          className="bg-white p-2 rounded-md shadow-lg md:w-3/5"
-          draggable={false}
-          src={img[2]}
-          alt="rate"
-        />
-        <img
-          className="bg-white p-2 rounded-md shadow-lg md:w-3/5"
-          draggable={false}
-          src={img[3]}
-          alt="rate"
-        />
-        <img
-          className="bg-white p-2 rounded-md shadow-lg md:w-3/5"
-          draggable={false}
-          src={img[4]}
-          alt="rate"
-        />
+        {img.map((img, index) => (
+          <img
+            key={index}
+            className="bg-white p-2 rounded-md shadow-lg md:w-3/5"
+            draggable={false}
+            src={img}
+            alt="rate"
+          />
+        ))}
       </div>
     </section>
   );
