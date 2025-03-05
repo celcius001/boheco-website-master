9fa8beb73fa4ab3d4ce844b528f2de16fd3bfe4d
celcius001 <itscelciusgaming@gmail.com>
2025-03-05 09:02:16 +0800

    update gen charge feb 2025

--- src/components/GenRates.jsx
+++ src/components/GenRates.jsx
@@ -6,7 +6,7 @@ const img = [
   "assets/rates/gr2.png",
 ];
 
-const months = ["January", "December", "November"];
+const months = ["February", "January", "December"];
 
 const Rates = () => {
   return (
@@ -18,7 +18,7 @@ const Rates = () => {
         <img draggable={false} src={img[0]} alt="rate" />
       </div>
       <div className="flex items-center justify-center text-xl py-12 md:text-2xl lg:text-4xl font-[Gloock] font-extrabold lg:py-12 uppercase">
-        Breakdown Of Generation Charge For The Month Of {months[1]} 2024
+        Breakdown Of Generation Charge For The Month Of {months[1]} 2025
       </div>
       <div className="flex flex-col items-center justify-center">
         <img draggable={false} src={img[1]} alt="rate" />
